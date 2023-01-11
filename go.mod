0ff093871f2e75f61c65e03c476100cca139469b
kzame <ludovic.surel@gmail.com>
2023-01-11 13:12:41 +0400

    creation d'un hello world pour tester si le webserveur fonctionne en local

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/kzame974/GoAPI
 
 go 1.20
+
+require github.com/gin-gonic/gin v1.8.2
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
--- src/main.go
+++ src/main.go
@@ -1,1 +1,24 @@
-package src
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kzame974/GoAPI/src/routes"
+	"log"
+	"os"
+)
+
+func main() {
+	// on définit notre routeur dans le main. Ceci est juste un test pour voir si l'API fonctionne
+	router := gin.New()
+	port := os.Getenv("PORT")
+
+	errProxy := router.SetTrustedProxies([]string{"192.168.1.2"})
+	if errProxy != nil {
+		log.Fatal(errProxy)
+	}
+	routes.UserRoute(router)
+	err := router.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
